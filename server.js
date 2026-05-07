56a445f8db266f23b348ed29efabe0da98605444
sdpeterraja <43093239+sdpeterraja@users.noreply.github.com>
2026-05-07 21:38:21 +0530

    Initial backend deployment2

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST"],
-  credentials: true,
+    origin: 'https://ccass-prototype.kaleyra.dev',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
