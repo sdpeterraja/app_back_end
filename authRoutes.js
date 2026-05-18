f1319b96a0704d0a8d1313e9a01f5187fadeb9e0
sdpeterraja <43093239+sdpeterraja@users.noreply.github.com>
2026-05-18 06:29:04 +0530

    Update authRoutes.js

--- authRoutes.js
+++ authRoutes.js
@@ -62,7 +62,7 @@ router.post("/users", authenticate, requireRole("admin"), (req, res) => {
   }
   try {
     createUser(username, password, role);
-    res.status(201).json({ message: User '${username}' created with role '${role}' });
+    res.status(201).json({ message: `User '${username}' created with role '${role}'` });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
